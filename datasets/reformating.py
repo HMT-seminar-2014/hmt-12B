0b08a3b5895a87508ec94a3c106f2aa54cd71ae0
SamuelHill <whatinthesam@gmail.com>
2014-06-30 16:51:58 -0400

    tsv stuff

--- datasets/reformating.py
+++ datasets/reformating.py
@@ -1,12 +1,12 @@
 import csv
 import re
 
-sCite = '/Users/SamHill/Documents/hmt-vm/hmt-12B/datasets/scholia-citations.txt'
-sPeName = '/Users/SamHill/Documents/hmt-vm/hmt-12B/datasets/scholia-persName-index.txt'
-sPlName = '/Users/SamHill/Documents/hmt-vm/hmt-12B/datasets/scholia-placeNames.txt'
+sCite = '/Users/SamHill/Documents/hmt-vm/hmt-12B/datasets/scholia-citations.tsv'
+sPeName = '/Users/SamHill/Documents/hmt-vm/hmt-12B/datasets/scholia-persName-index.tsv'
+sPlName = '/Users/SamHill/Documents/hmt-vm/hmt-12B/datasets/scholia-placeNames.tsv'
 
 file = open(sCite, "rU")
-reader = csv.reader(file, delimiter='\t')
+reader = csv.reader(file) #, delimiter='\t'
 for row in reader:
-	print '"%s","%s"' % (row[0], row[1])
+	print '%s\t%s' % (row[0], row[1])
 file.close()
